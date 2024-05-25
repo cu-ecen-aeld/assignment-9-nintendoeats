e5a533053976b85271094ec78782d1e9c177b48d
nintendoeats <adrian@hallmonitor.ca>
2024-05-25 15:28:56 -0400

    Completed assignment 9

--- base_external/package/aesd-char/aesd-char.mk
+++ base_external/package/aesd-char/aesd-char.mk
@@ -5,7 +5,7 @@
 #
 ##############################################################
 
-AESD_CHAR_VERSION = bd99a56204c2c92395a08614627e81f8ea5912b2
+AESD_CHAR_VERSION = 133a38e866aca25f32cc72533422277fc5d420c4
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
